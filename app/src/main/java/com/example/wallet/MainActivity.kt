92a7e154750f240d5eb17c80d1af809d9065d3f1
visnyaisoma <visnyaisoma999@gmail.com>
2021-10-19 20:56:45 +0200

    Code shake detection, delete your items in your wallet when shake occurs Add seismic library to build.gradle.

--- app/src/main/java/com/example/wallet/MainActivity.kt
+++ app/src/main/java/com/example/wallet/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.wallet
 
+import android.content.Context
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.example.wallet.databinding.ActivityMainBinding
 import com.example.wallet.databinding.SalaryRowBinding
+import com.squareup.seismic.ShakeDetector
 
-class MainActivity(): AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var rowBinding: SalaryRowBinding
     private var sum = 0
+    private lateinit var sensorManager: SensorManager
+    private lateinit var shakeDetector: ShakeDetector
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,21 +27,32 @@ class MainActivity(): AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        shakeDetector = ShakeDetector(ShakeDetector.Listener {
+
+            Toast.makeText(this, "Your items are cleared!", Toast.LENGTH_SHORT)
+                .show()
+            // clear listOfRows
+            binding.listOfRows.removeAllViews()
+            binding.sumTextview.text = ""
+
+        })
+
 
         binding.saveButton.setOnClickListener {
-            if (binding.salaryName.text.toString().isEmpty() || binding.salaryAmount.text.toString().isEmpty()) {
+            if (binding.salaryName.text.toString().isEmpty() || binding.salaryAmount.text.toString()
+                    .isEmpty()
+            ) {
                 val contextView = findViewById<View>(R.id.list_of_rows)
                 Snackbar.make(contextView, R.string.warn_message, Snackbar.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-                if(binding.expenseOrIncome.isChecked){
-                    sum = sum - binding.salaryAmount.text.toString().toInt()
-                } else{
-                    sum = sum + binding.salaryAmount.text.toString().toInt()
+            if (binding.expenseOrIncome.isChecked) {
+                sum = sum - binding.salaryAmount.text.toString().toInt()
+            } else {
+                sum = sum + binding.salaryAmount.text.toString().toInt()
             }
             binding.sumTextview.text = sum.toString()
-
-
 
 
             rowBinding = SalaryRowBinding.inflate(layoutInflater)
@@ -64,6 +81,18 @@ class MainActivity(): AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+        shakeDetector.start(sensorManager)
+    }
+
+    override fun onPause() {
+
+        super.onPause()
+        shakeDetector.stop()
     }
 
 
